b5ed9c772a610b7960843798511d90c1c1b0efba
Vibhav Lal <vib.lal@gmail.com>
2018-07-15 01:23:25 -0700

    update report and project

--- mlnd/projects/capstone/Gold-Spot-Price/visualize.py
+++ mlnd/projects/capstone/Gold-Spot-Price/visualize.py
@@ -34,7 +34,31 @@ def plot_basic(commodity, title='Gold Spot Prices', y_label='Price USD', x_label
 
     plt.show()
 
+def plot_basic_features(commodity, title='Gold Spot Prices with all features', y_label='Price USD', x_label='Trading Days'):
+    """
+    Plots basic pyplot
+    :param stocks: DataFrame having all the necessary data
+    :param title:  Title of the plot 
+    :param y_label: yLabel of the plot
+    :param x_label: xLabel of the plot
+    :return: prints a Pyplot againts items and their closing value
+    """
+    fig, ax = plt.subplots()
+    ax.plot(commodity['Item'], commodity['Close'], '#0A7388')
+    ax.plot(commodity['Item'], commodity['USD1MTD156N'], '#0A7388')
+    ax.plot(commodity['Item'], commodity['Silver'], '#0A7388')
+    ax.plot(commodity['Item'], commodity['DGASNYH'], '#0A7388')
+    ax.plot(commodity['Item'], commodity['DEXCHUS'], '#0A7388')
+   
+    ax.format_ydata = price
+    ax.set_title(title)
 
+    # Add labels
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+
+    plt.show()
+    
 def plot_prediction(actual, prediction, title='Gold Spot Price vs Prediction', y_label='Price USD', x_label='Trading Days'):
     """
     Plots train, test and prediction
